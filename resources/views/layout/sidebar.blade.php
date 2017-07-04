b06a256961dc85ae84b0ac4e43d2ae042f50f7bc
9bombs <b_nj18@live.com>
2017-07-04 13:17:14 +0700

    Test

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,144 @@
+
+
+<div class="col-md-3 left_col">
+        <div class="left_col scroll-view">
+          <div class="navbar nav_title" style="border: 0;">
+            <a href="index.html" class="site_title"><span>Executive Report</span></a>
+          </div>
+
+          <div class="clearfix"></div>
+
+          <!-- menu profile quick info -->
+          <div class="profile clearfix">
+            <div class="profile_pic">
+              <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+            </div>
+            <div class="profile_info">
+              <span>ยินดีต้อนรับ,</span>
+              <h2>ณัฐพล เจริญชัย</h2>
+            </div>
+          </div>
+          <!-- /menu profile quick info -->
+
+          <br />
+
+          <!-- sidebar menu -->
+          <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+            <div class="menu_section">
+              <h3>เมนูหลัก</h3>
+              <ul class="nav side-menu">
+
+                <li><a href="index.html"><i class="fa fa-home"></i> แผงควบคุม </span></a></li>
+                <li><a><i class="fa fa-home"></i> ประสานงานและเครือข่าย <span class="fa fa-chevron-down"></span></a>
+                  <ul class="nav child_menu">
+                    <li>
+                      <a href="cooperate.html">รายงานภาพรวม</a>
+                    </li>
+                    <li>
+                      <a href="cooperate-form.html">นำเข้าข้อมูล</a>
+                    </li>
+                    <li>
+                      <a href="act-form.html">สร้างกิจกรรมใหม่</a>
+                    </li>
+                  </ul>
+                </li>
+
+                <li><a><i class="fa fa-edit"></i> พัฒนานิสิต <span class="fa fa-chevron-down"></span></a>
+
+                  <ul class="nav child_menu">
+                    <li>
+                      <a href="development.html">รายงานภาพรวม</a>
+                    </li>
+                    <li>
+                      <a href="development-form.html">นำเข้าข้อมูล</a>
+                    </li>
+                    <li>
+                      <a href="act-form.html">สร้างกิจกรรมใหม่</a>
+                    </li>
+                  </ul>
+                </li>
+
+                <li><a><i class="fa fa-sitemap"></i> ทุนและบริการนิสิต <span class="fa fa-chevron-down"></span></a>
+
+                  <ul class="nav child_menu">
+                    <li>
+                      <a href="scholarship.html">รายงานภาพรวม</a>
+                    </li>
+                    <li>
+                      <a href="scholarship-data.html">รายชื่อนิสิตทุน</a>
+                    </li>
+                    <li>
+                      <a href="scholarship-new-form.html">เพิ่มทุนภายนอกใหม่</a>
+                    </li>
+                    <li>
+                      <a href="act-form.html">สร้างกิจกรรมใหม่</a>
+                    </li>
+                    <li>
+                      <a href="scholarship-report-data.html">นำเข้าข้อมูลรายงานรายเดือน</a>
+                    </li>
+                  </ul>
+                </li>
+
+                <li><a><i class="fa fa-sitemap"></i> หอพักนิสิต <span class="fa fa-chevron-down"></span></a>
+
+                  <ul class="nav child_menu">
+                    <li>
+                      <a href="residence.html">รายงานภาพรวม</a>
+                    </li>
+                    <li>
+                      <a href="act-form.html">สร้างกิจกรรมใหม่</a>
+                    </li>
+                    <li>
+                      <a href="residence-report-data.html">นำเข้าข้อมูลรายงานรายเดือน</a>
+                    </li>
+                  </ul>
+                </li>
+
+                <li><a><i class="fa fa-sitemap"></i> ส่งเสริมสุขภาวะนิสิต <span class="fa fa-chevron-down"></span></a>
+
+                  <ul class="nav child_menu">
+                    <li>
+                      <a href="wellness.html">รายงานภาพรวม</a>
+                    </li>
+                    <li>
+                      <a href="wellness-data.html">ผู้ใช้บริการทั้งหมด</a>
+                    </li>
+                    <li>
+                      <a href="wellness-details.html">ข้อมูลผู้เข้าใช้บริการ</a>
+                    </li>
+                    <li>
+                      <a href="wellness-new-form.html">เพิ่มผู้ใช้บริการใหม่</a>
+                    </li>
+                    <li>
+                      <a href="act-form.html">สร้างกิจกรรมใหม่</a>
+                    </li>
+                    <li>
+                      <a href="wellness-report-data.html">นำเข้าข้อมูลรายงานรายเดือน</a>
+                    </li>
+                  </ul>
+                </li>
+
+              </ul>
+            </div>
+
+          </div>
+          <!-- /sidebar menu -->
+
+          <!-- /menu footer buttons -->
+          <div class="sidebar-footer hidden-small">
+            <a data-toggle="tooltip" data-placement="top" title="Settings">
+                            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+                        </a>
+            <a data-toggle="tooltip" data-placement="top" title="FullScreen">
+                            <span class="glyphicon glyphicon-fullscreen" aria-hidden="true"></span>
+                        </a>
+            <a data-toggle="tooltip" data-placement="top" title="Lock">
+                            <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
+                        </a>
+            <a data-toggle="tooltip" data-placement="top" title="Logout">
+                            <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+                        </a>
+          </div>
+          <!-- /menu footer buttons -->
+        </div>
+      </div>
